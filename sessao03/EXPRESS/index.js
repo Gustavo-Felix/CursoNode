e8ed5140f3b6967bb844ac0228d4d3ef8902650c
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-04 06:42:01 -0300

    Parâmetros

--- sessao03/EXPRESS/index.js
+++ sessao03/EXPRESS/index.js
@@ -2,26 +2,43 @@ const express = require('express'); // importando o express
 const app = express(); // iniciando o express
 
 app.get("/", (req, res) => {
-        res.send("<h1>Bem-vindo!</h1>")
-    }
-);
+    res.send("<h1>Bem-vindo!</h1>");
+    
+});
 
-app.get("/blog", (req, res) => {
-        res.send("Bem-vindo ao blog!")
+app.get("/blog/:artigo?", (req, res) => {
+
+    var artigo = req.params.artigo
+
+    if(artigo) {
+        res.send("<h2>Artigo: " + artigo + "</h2>");
     }
-);
+    else{
+        res.send("<h1>Bem-vindo ao Blog!</h1>");
+    }
+    
+});
 
 app.get("/post", (req, res) => {
-    res.send("Bem-vindo ao meu post!")
-}
-);
+    res.send("Bem-vindo ao meu post!");
+
+});
+
+app.get("/ola/:nome/:empresa", (req, res) => {
+    // req => Dados enviados pelo usuário
+    // res => Resposta do servidor para o usuário
+    var nome = req.params.nome
+    var empresa = req.params.empresa
+    res.send('<h1>Oi ' + nome + ' do ' + empresa + ' </h1>');
+
+});
 
 app.listen(8080, (err) => {
     if(err){
-        console.log('Ocorreu um erro!', err)
+        console.log('Ocorreu um erro!', err);
     }
     else{
-        console.log('Servidor iniciado com sucesso!')
+        console.log('Servidor iniciado com sucesso!');
     }
-})
+});
 
