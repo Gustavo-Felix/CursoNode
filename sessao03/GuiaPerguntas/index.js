a2e170dd5bbb79d613c132f9ec7cbccb9bf6bae8
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-05 07:35:37 -0300

    Estruturas condicionais com EJS

--- sessao03/GuiaPerguntas/index.js
+++ sessao03/GuiaPerguntas/index.js
@@ -6,10 +6,12 @@ app.set('view engine', 'ejs'); // Estou dizendo para usar o EJS como View Engine
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var ExibirMsg = false;
     res.render('index.ejs',
         {
             nome: nome,
             lang: lang,
+            msg: ExibirMsg,
         }
     );
 
