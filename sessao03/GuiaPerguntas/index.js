e6514b8261711e28b21837b772bbecaf12af814b
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-05 08:06:05 -0300

    Arquivos estáticos e Bootstrap

--- sessao03/GuiaPerguntas/index.js
+++ sessao03/GuiaPerguntas/index.js
@@ -2,28 +2,10 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs'); // Estou dizendo para usar o EJS como View Engine
+app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var ExibirMsg = false;
-    var Produtos = [
-        {nome: 'Doritos', preco: 7.99},
-        {nome: 'Playstation', preco: 3599.99},
-        {nome: 'Xbox', preco: 2999.99},
-        {nome: 'Notebook', preco: 3499.99},
-        {nome: 'Computador', preco: 6999.99},
-    ]
-
-    res.render('index.ejs',
-        {
-            nome: nome,
-            lang: lang,
-            msg: ExibirMsg,
-            produtos: Produtos,
-        }
-    );
-
+app.get("/", (req, res) => {
+    res.render('index.ejs');
 });
 
 app.get("/home", (req, res) => {
