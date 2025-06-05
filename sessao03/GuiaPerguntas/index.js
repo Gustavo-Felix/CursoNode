93ec3a9a57a3826d9e5e59b776a39aa5b56d679d
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-05 07:23:27 -0300

    Configurando e exibindo HTML com EJS

---
+++ sessao03/GuiaPerguntas/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs'); // Estou dizendo para usar o EJS como View Engine
+
+app.get("/", (req, res) => {
+    res.render('index.ejs');
+
+});
+
+app.get("/home", (req, res) => {
+    res.render('home.ejs');
+
+});
+
+app.listen(8080, () => {
+    console.log('App Rodando');
+
+});
