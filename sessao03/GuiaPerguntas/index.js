435d29d8b5e93b9afa73f50a9fde3bd3af5ba4d8
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-05 07:43:33 -0300

    Estruturas de repetição com EJS

--- sessao03/GuiaPerguntas/index.js
+++ sessao03/GuiaPerguntas/index.js
@@ -7,11 +7,20 @@ app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
     var ExibirMsg = false;
+    var Produtos = [
+        {nome: 'Doritos', preco: 7.99},
+        {nome: 'Playstation', preco: 3599.99},
+        {nome: 'Xbox', preco: 2999.99},
+        {nome: 'Notebook', preco: 3499.99},
+        {nome: 'Computador', preco: 6999.99},
+    ]
+
     res.render('index.ejs',
         {
             nome: nome,
             lang: lang,
             msg: ExibirMsg,
+            produtos: Produtos,
         }
     );
 
