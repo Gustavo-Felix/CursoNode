685961e6d5d16fb69361e678de749a6397427ae6
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-06-09 08:14:52 -0300

    Gerando tabela com model

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CursoNode
 Curso de Node.js
+
+Projeto Sessão 03 - Guia Perguntas
+
+    * É necessário o uso de MySQL Workbench e a biblioteca Sequelize para a manipulação de dados, utilize o arquivo 'EX_database.js' para a configuração correta.
---
+++ sessao03/GuiaPerguntas/database/Pergunta.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const connection = require('./database');
+
+const Pergunta = connection.define('perguntas', { // Cria a tabela no MySQL utilizando Javascript - Tabela pergunta, com os valores title (String) e description (String)
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+});
+
+Pergunta.sync({force: false}).then(() => {
+    console.log('Tabela criada com sucesso!');
+
+}); // Se não existir a tabela pergunta, ela será criada
