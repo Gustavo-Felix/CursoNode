3f394a20c2f597571885ccf1a44345d6d423fac4
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-07-07 08:08:58 -0300

    Tratamento de erros em callbacks

--- sessao09/callbacks/index.js
+++ sessao09/callbacks/index.js
@@ -1,5 +1,6 @@
 function sendEmail(corpo, para, callback){
     setTimeout(() => {
+        var Error = false;
         console.log(`
             Para: ${para}
             ----------------------------
@@ -7,11 +8,19 @@ function sendEmail(corpo, para, callback){
             ----------------------------
             De: Gustavo Félix
             `);
-        callback();
+        if(Error){
+            callback("O envio do email falhou!");
+        }else{
+            callback();
+        }
     }, 8000);
 }
 
 console.log("Seu e-mail foi enviado com sucesso!");
-sendEmail("Oi, este é um teste de programação assíncrona.", "gustavo", () => {
-    console.log("Enviado!");
+sendEmail("Oi, este é um teste de programação assíncrona.", "gustavo", (erro) => {
+    if(erro == undefined){
+        console.log("Tudo OK!");
+    }else {
+        console.log("Ocorreu um erro:" + erro);
+    }
 });
