00ebb8591d951f674ac4a2e338992f6aa43b974b
Gustavo-Félix <gustavofelix2007@gmail.com>
2025-07-08 08:15:28 -0300

    Métodos estáticos

--- sessao10/class.js
+++ sessao10/class.js
@@ -1,16 +1,37 @@
 class Jogo{
     constructor(titulo, genero, produtora, preco){
-        this.titulo = titulo
-        this.genero = genero
-        this.produtora = produtora
-        this.preco = preco
+        this.titulo = titulo;
+        this.genero = genero;
+        this.produtora = produtora;
+        this.preco = preco;
     }
 
     Abrir(){
-        console.log(`O ${this.titulo} está sendo aberto.`)
+        console.log(`O ${this.titulo} está sendo aberto.`);
+    }
+
+    Fechar(){
+        console.log(`O ${this.titulo} está sendo fechado.`);
+    }
+
+    static Isso(){
+        console.log("Classe Jogo")
+    }
+
+    Ficha(){
+        console.log(`
+            Titulo: ${this.titulo}
+            Gênero: ${this.genero}
+            Produtora: ${this.produtora}
+            Preço: ${this.preco}
+        `);
+        this.Fechar();
     }
 }
 
-cs = new Jogo("Counter-Strike", "FPS", "Valve", 0)
+Jogo.Isso();
 
-cs.Abrir()
+cs = new Jogo("Counter-Strike", "FPS", "Valve", 0);
+
+cs.Abrir();
+cs.Ficha();
